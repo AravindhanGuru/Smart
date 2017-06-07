1d83a1d5ce8f8a91011d17d7790a3daa338dd4ea
AravindhanGuru <aravindhansmart007@gmail.com>
2017-06-07 12:29:15 +0530

    Create hell0.java

---
+++ hell0.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Sample1 {
+
+   
+    public static void main(String[] args) {
+       
+        
+    {
+    
+Scanner in=new Scanner(System.in);
+
+for(int i=0;i<5;i++)
+{
+    System.out.println("Hello");
+}
+
+
+}
+    }
+}
