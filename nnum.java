506dd0c765472d0cbff4aa212fba8e58e11c033f
AravindhanGuru <aravindhansmart007@gmail.com>
2017-06-08 17:06:04 +0530

    Create nnum.java

---
+++ nnum.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class nnum
+{
+public Static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+int sum=0;
+int n=in.nextInt();
+for(int i=0;i<n;i++)
+{
+sum=sum+i;
+}
+System.out.println(sum);
+}
+}
