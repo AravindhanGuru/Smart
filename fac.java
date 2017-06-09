e34f98ecca340d1568dfec9331571245709df639
AravindhanGuru <aravindhansmart007@gmail.com>
2017-06-09 18:02:41 +0530

    Create fac.java

---
+++ fac.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class fac
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int fact=1;
+        for(int i=1;i<=n;i++)
+        {
+            fact=fact*i;
+              
+            }
+           System.out.println(fact);
+        
+    }
+}
