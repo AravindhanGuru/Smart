dc3f2b80af2c3b8c6b89c2d0cd3623c168db096a
AravindhanGuru <aravindhansmart007@gmail.com>
2017-06-09 18:09:52 +0530

    Create table.java

---
+++ table.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class table
+{
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        
+        for(int i=0;i<=10;i++)
+        {
+        System.out.println(n+"*"+i+"="+n*i);
+              
+            }
+    }
+}
