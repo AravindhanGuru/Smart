712d5e7bd2392fe95fefd2c37ea0f60ab9a9a627
AravindhanGuru <aravindhansmart007@gmail.com>
2017-06-19 20:50:23 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+class ReverseString
+{
+    public static void main(String[] args)
+    {
+        Scanner in=new Scanner(System.in);    
+        String s=in.nextLine();
+     
+        char[] n = s.toCharArray();
+ 
+        for (int i = n.length-1; i>=0; i--)
+            System.out.print(try1[i]);
+    }
+}
